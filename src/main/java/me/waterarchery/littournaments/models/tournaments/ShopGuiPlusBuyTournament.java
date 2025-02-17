750fce323a045fc1c310bca2800332145fe162a7
Thiago Gebrim <thiagogebrim@hotmail.com>
2025-02-16 23:36:30 -0300

    add SHOPGUIPLUS_BUY_TOURNAMENT

---
+++ src/main/java/me/waterarchery/littournaments/models/tournaments/ShopGuiPlusBuyTournament.java
@@ -0,0 +1,12 @@
+package me.waterarchery.littournaments.models.tournaments;
+
+import me.waterarchery.littournaments.models.Tournament;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+public class ShopGuiPlusBuyTournament extends Tournament {
+
+    public ShopGuiPlusBuyTournament(String identifier, YamlConfiguration yamlConfiguration) {
+        super(identifier, yamlConfiguration);
+    }
+
+}
